b0df43f89cf81f31acce5d252e6f59a60709663f
Saint99599 <82707210+Saint99599@users.noreply.github.com>
2021-07-09 01:45:03 +0700

    Add files via upload

---
+++ js/close.js
@@ -0,0 +1,224 @@
+document.getElementById('buylottery00').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery01').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery02').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery03').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery04').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery05').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery06').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery07').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery08').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery09').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery10').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery11').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery12').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery13').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery14').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery15').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery16').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery17').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery18').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery19').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery20').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery21').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery22').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery23').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
+
+document.getElementById('buylottery24').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.confirmpopup').style.display = 'none';
+});
